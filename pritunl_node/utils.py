4d993f0e624435791be466b8fbdc26c5a110db00
Zachary Huff <zach.huff.386@gmail.com>
2014-01-14 02:45:03 -0500

    Add check output to utils

--- pritunl_node/utils.py
+++ pritunl_node/utils.py
@@ -2,3 +2,19 @@ import subprocess
 
 def rmtree(path):
     subprocess.check_call(['rm', '-r', path])
+
+def check_output(*popenargs, **kwargs):
+    # For python2.6 support
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, ' + \
+            'it will be overridden.')
+    process = subprocess.Popen(stdout=subprocess.PIPE,
+        *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get('args')
+        if cmd is None:
+            cmd = popenargs[0]
+        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    return output
