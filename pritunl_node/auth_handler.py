006d186e04836bd943a7dce27ccfd992a16ebc78
Zachary Huff <zach.huff.386@gmail.com>
2014-02-05 18:16:34 -0500

    Add local auth handler

--- pritunl_node/auth_handler.py
+++ pritunl_node/auth_handler.py
@@ -5,3 +5,8 @@ class AuthHandler(tornado.web.RequestHandler):
     def prepare(self):
         if self.request.headers.get('API-Key') != app_server.api_key:
             raise tornado.web.HTTPError(401)
+
+class AuthLocalHandler(tornado.web.RequestHandler):
+    def prepare(self):
+        if self.request.remote_ip not in ['127.0.0.1', '::1']:
+            raise tornado.web.HTTPError(401)
