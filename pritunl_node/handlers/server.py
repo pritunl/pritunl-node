c7b3ef7b427b2abd37e13dc2d976cd55321fcc2b
Zachary Huff <zach.huff.386@gmail.com>
2014-02-05 18:16:51 -0500

    Improve server handlers

--- pritunl_node/handlers/server.py
+++ pritunl_node/handlers/server.py
@@ -1,6 +1,6 @@
 from pritunl_node.constants import *
 from pritunl_node.server import Server
-from pritunl_node.auth_handler import AuthHandler
+from pritunl_node.auth_handler import AuthHandler, AuthLocalHandler
 from pritunl_node import app_server
 import tornado.ioloop
 import tornado.web
@@ -42,31 +42,28 @@ class ServerHandler(AuthHandler):
         })
 app_server.app.add_handlers('.*', [(r'/server/([a-z0-9]+)', ServerHandler)])
 
-class ServerTestHandler(AuthHandler):
-    @tornado.web.asynchronous
-    def get(self, server_id):
-        self.finish('test')
-app_server.app.add_handlers('.*', [(r'/server/([a-z0-9]+)/test',
-    ServerTestHandler)])
+class ServerTlsVerifyHandler(AuthLocalHandler):
+    timeout = None
+    call_id = None
 
-class ServerTlsVerifyHandler(AuthHandler):
     @tornado.web.asynchronous
     def post(self, server_id):
         data = tornado.escape.json_decode(self.request.body)
         org_id = data['org_id']
         user_id = data['user_id']
         server = Server(id=server_id)
-        self.timeout = None
-        self.call_id = None
         self.call_buffer = server.call_buffer
 
         self.timeout = tornado.ioloop.IOLoop.current().add_timeout(
-            time.time() + CALL_RESPONSE_TIMEOUT, self.on_response)
+            time.time() + CALL_RESPONSE_TIMEOUT, self.on_timeout)
 
         self.call_id = self.call_buffer.create_call('tls_verify',
             [org_id, user_id], self.on_response)
 
-    def on_response(self, authenticated=False):
+    def on_timeout(self):
+        self.send_error(504)
+
+    def on_response(self, authenticated):
         if self.request.connection.stream.closed():
             return
         self.finish({
@@ -81,7 +78,10 @@ class ServerTlsVerifyHandler(AuthHandler):
 app_server.app.add_handlers('.*', [(r'/server/([a-z0-9]+)/tls_verify',
     ServerTlsVerifyHandler)])
 
-class ServerOtpVerifyHandler(AuthHandler):
+class ServerOtpVerifyHandler(AuthLocalHandler):
+    timeout = None
+    call_id = None
+
     @tornado.web.asynchronous
     def post(self, server_id):
         data = tornado.escape.json_decode(self.request.body)
@@ -89,17 +89,18 @@ class ServerOtpVerifyHandler(AuthHandler):
         user_id = data['user_id']
         otp_code = data['otp_code']
         server = Server(id=server_id)
-        self.timeout = None
-        self.call_id = None
         self.call_buffer = server.call_buffer
 
         self.timeout = tornado.ioloop.IOLoop.current().add_timeout(
-            time.time() + CALL_RESPONSE_TIMEOUT, self.on_response)
+            time.time() + CALL_RESPONSE_TIMEOUT, self.on_timeout)
 
         self.call_id = self.call_buffer.create_call('otp_verify',
             [org_id, user_id, otp_code], self.on_response)
 
-    def on_response(self, authenticated=False):
+    def on_timeout(self):
+        self.send_error(504)
+
+    def on_response(self, authenticated):
         if self.request.connection.stream.closed():
             return
         self.finish({
@@ -114,35 +115,83 @@ class ServerOtpVerifyHandler(AuthHandler):
 app_server.app.add_handlers('.*', [(r'/server/([a-z0-9]+)/otp_verify',
     ServerOtpVerifyHandler)])
 
-class ServerClientConnectHandler(AuthHandler):
+class ServerClientConnectHandler(AuthLocalHandler):
+    timeout = None
+    call_id = None
+
+    @tornado.web.asynchronous
     def post(self, server_id):
         data = tornado.escape.json_decode(self.request.body)
         org_id = data['org_id']
         user_id = data['user_id']
         server = Server(id=server_id)
+        self.call_buffer = server.call_buffer
 
+        self.timeout = tornado.ioloop.IOLoop.current().add_timeout(
+            time.time() + CALL_RESPONSE_TIMEOUT, self.on_timeout)
+
+        self.call_id = self.call_buffer.create_call('client_connect',
+            [org_id, user_id], self.on_response)
+
+    def on_timeout(self):
+        self.send_error(504)
+
+    def on_response(self, client_conf):
+        if self.request.connection.stream.closed():
+            return
         self.finish({
-            'client_conf': None,
+            'client_conf': client_conf,
         })
+
+    def on_finish(self):
+        if self.call_id:
+            self.call_buffer.cancel_call(self.call_id)
+        if self.timeout:
+            tornado.ioloop.IOLoop.current().remove_timeout(self.timeout)
 app_server.app.add_handlers('.*', [(r'/server/([a-z0-9]+)/client_connect',
     ServerClientConnectHandler)])
 
-class ServerClientDisconnectHandler(AuthHandler):
+class ServerClientDisconnectHandler(AuthLocalHandler):
+    timeout = None
+    call_id = None
+
+    @tornado.web.asynchronous
     def post(self, server_id):
         data = tornado.escape.json_decode(self.request.body)
         org_id = data['org_id']
         user_id = data['user_id']
         server = Server(id=server_id)
+        self.call_buffer = server.call_buffer
 
+        self.timeout = tornado.ioloop.IOLoop.current().add_timeout(
+            time.time() + CALL_RESPONSE_TIMEOUT, self.on_timeout)
+
+        self.call_id = self.call_buffer.create_call('client_disconnect',
+            [org_id, user_id], self.on_response)
+
+    def on_timeout(self):
+        self.send_error(504)
+
+    def on_response(self, response):
+        if self.request.connection.stream.closed():
+            return
         self.finish({})
+
+    def on_finish(self):
+        if self.call_id:
+            self.call_buffer.cancel_call(self.call_id)
+        if self.timeout:
+            tornado.ioloop.IOLoop.current().remove_timeout(self.timeout)
 app_server.app.add_handlers('.*', [(r'/server/([a-z0-9]+)/client_disconnect',
     ServerClientDisconnectHandler)])
 
 class ServerComHandler(AuthHandler):
+    call_buffer = None
+    timeout = None
+
     @tornado.web.asynchronous
     def put(self, server_id):
         server = Server(id=server_id)
-        self.timeout = None
         self.call_buffer = server.call_buffer
 
         if not self.call_buffer:
